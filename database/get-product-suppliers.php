5a4f4b973ad45957b3a50d28f075e90a3a0e6713
Luis Gabriel García Mingall <90941271+luisga88gm@users.noreply.github.com>
2024-02-19 19:31:50 -0300

    Add files via upload

---
+++ database/get-product-suppliers.php
@@ -0,0 +1,11 @@
+<?php
+include('connection.php');
+
+$id = $_GET['id'];
+
+//Fetch Suppliers.
+$stmt = $conn->prepare("SELECT supplier_name, suppliers.id FROM suppliers, productsuppliers WHERE productsuppliers.product=$id AND productsuppliers.supplier = suppliers.id");
+$stmt->execute();
+$suppliers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($suppliers);
---
+++ login.php
@@ -0,0 +1,62 @@
+<?php
+    //Start the session.
+    session_start();
+    if(isset($_SESSION['user'])) header('location: dashboard.php');
+
+    if($_POST){
+        include('database/connection.php');
+        
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $query = 'SELECT * FROM users WHERE users.email="'. $username .'" AND users.password="'. $password .'"';
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+
+        if($stmt->rowCount() > 0){
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $user = $stmt->fetchAll()[0];
+
+            //Captures data of currently login users.
+            $_SESSION['user'] = $user;
+
+            header('Location: dashboard.php');
+        } else $error_message = 'Please make sure that username and password are correct.';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>IMS Login - Inventory Management System</title>
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body id="loginBody">
+    <?php if(!empty($error_message)) { ?>
+        <div id="errorMessage">
+            <strong>ERROR:</strong> </p><?= $error_message ?> </p>
+        </div>
+    <?php } ?>
+    <div class="container">
+        <div class="loginHeader">
+            <h1>IMS</h1>
+            <p>Inventory Management System</p>
+        </div>
+        <div class="loginBody">
+            <form action="login.php" method="POST">
+                <div class="loginInputsContainer">
+                    <label for="">Username</label>
+                    <input placeholder="username" name="username" type="text" />
+                </div>
+                <div class="loginInputsContainer">
+                    <label for="">Password</label>
+                    <input placeholder="password" name="password" type="password" />
+                </div>
+                <div class="loginButtonContainer">
+                    <button>login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
